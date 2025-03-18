51d995f3636e48f6aa27d8a67d44a27dfac8635b
Vereshchynskyi Nazar <verechnazar12@gmail.com>
2025-03-18 02:00:27 +0200

    v1.0.1

--- reservations.js
+++ reservations.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const container = document.querySelector(".cards-container");
-
+    let container = document.querySelector(".cards-container");
     let reservedTours = JSON.parse(localStorage.getItem("reservedTours")) || [];
 
     reservedTours.forEach(tour => {
-        const card = document.createElement("div");
+        let card = document.createElement("div");
+
         card.classList.add("card");
         card.innerHTML = `
             <img class="${tour.image_class}" src="${tour.image}" alt="${tour.name}">
@@ -24,12 +24,11 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
-// Функція для видалення туру з localStorage
 function deleteTour(tourName) {
     let reservedTours = JSON.parse(localStorage.getItem("reservedTours")) || [];
+
     reservedTours = reservedTours.filter(t => t.name !== tourName);
     localStorage.setItem("reservedTours", JSON.stringify(reservedTours));
 
-    // Перезавантажуємо сторінку, щоб оновити список
     location.reload();
 }
